5e5106df9b01414aaa334046b2e886d4fd5c9201
felix herrmann <felix.rodau@gmail.com>
2025-06-03 15:28:44 +0200

    fix warnings

--- models/cost_volume.py
+++ models/cost_volume.py
@@ -4,7 +4,9 @@ from models.module import *
 # from models.swin.block import SwinTransformerCostReg
 import torch.utils.checkpoint as cp
 
+
 class identity_with(object):
+
     def __init__(self, enabled=True):
         self._enabled = enabled
 
@@ -19,13 +21,16 @@ autocast = torch.cuda.amp.autocast if torch.__version__ >= '1.6.0' else identity
 
 
 class StageNet(nn.Module):
+
     def __init__(self, args, ndepth, stage_idx):
         super(StageNet, self).__init__()
         self.args = args
         self.fusion_type = args.get('fusion_type', 'cnn')
         self.ndepth = ndepth
         self.stage_idx = stage_idx
-        self.cost_reg_type = args.get("cost_reg_type", ["Normal", "Normal", "Normal", "Normal"])[stage_idx]
+        self.cost_reg_type = args.get(
+            "cost_reg_type",
+            ["Normal", "Normal", "Normal", "Normal"])[stage_idx]
         self.depth_type = args["depth_type"]
         if type(self.depth_type) == list:
             self.depth_type = self.depth_type[stage_idx]
@@ -34,13 +39,17 @@ class StageNet(nn.Module):
         if type(in_channels) == list:
             in_channels = in_channels[stage_idx]
         if self.fusion_type == 'cnn':
-            self.vis = nn.Sequential(ConvBnReLU(1, 16), ConvBnReLU(16, 16), ConvBnReLU(16, 8), nn.Conv2d(8, 1, 1), nn.Sigmoid())
+            self.vis = nn.Sequential(ConvBnReLU(1, 16), ConvBnReLU(16, 16),
+                                     ConvBnReLU(16, 8), nn.Conv2d(8, 1, 1),
+                                     nn.Sigmoid())
         else:
-            raise NotImplementedError(f"Not implemented fusion type: {self.fusion_type}.")
+            raise NotImplementedError(
+                f"Not implemented fusion type: {self.fusion_type}.")
 
         if self.cost_reg_type == "PureTransformerCostReg":
             args['transformer_config'][stage_idx]['base_channel'] = in_channels
-            self.cost_reg = PureTransformerCostReg(in_channels, **args['transformer_config'][stage_idx])
+            self.cost_reg = PureTransformerCostReg(
+                in_channels, **args['transformer_config'][stage_idx])
         else:
             model_th = args.get('model_th', 8)
             if ndepth <= model_th:  # do not downsample in depth range
@@ -48,12 +57,19 @@ class StageNet(nn.Module):
             else:
                 self.cost_reg = CostRegNet(in_channels, in_channels)
 
-    def forward(self, features, proj_matrices, depth_values, tmp, position3d=None):
+    def forward(self,
+                features,
+                proj_matrices,
+                depth_values,
+                tmp,
+                position3d=None):
         ref_feat = features[:, 0]
         src_feats = features[:, 1:]
         src_feats = torch.unbind(src_feats, dim=1)
         proj_matrices = torch.unbind(proj_matrices, 1)
-        assert len(src_feats) == len(proj_matrices) - 1, "Different number of images and projection matrices"
+        assert len(src_feats) == len(
+            proj_matrices
+        ) - 1, "Different number of images and projection matrices"
 
         # step 1. feature extraction
         ref_proj, src_projs = proj_matrices[0], proj_matrices[1:]
@@ -61,15 +77,18 @@ class StageNet(nn.Module):
         volume_sum = 0.0
         vis_sum = 0.0
         similarities = []
-        with autocast(enabled=False):
+        with torch.amp.autocast('cuda', enabled=False):
             for src_feat, src_proj in zip(src_feats, src_projs):
                 # warpped features
                 src_feat = src_feat.to(torch.float32)
                 src_proj_new = src_proj[:, 0].clone()
-                src_proj_new[:, :3, :4] = torch.matmul(src_proj[:, 1, :3, :3], src_proj[:, 0, :3, :4])
+                src_proj_new[:, :3, :4] = torch.matmul(src_proj[:, 1, :3, :3],
+                                                       src_proj[:, 0, :3, :4])
                 ref_proj_new = ref_proj[:, 0].clone()
-                ref_proj_new[:, :3, :4] = torch.matmul(ref_proj[:, 1, :3, :3], ref_proj[:, 0, :3, :4])
-                warped_volume, proj_mask = homo_warping_3D_with_mask(src_feat, src_proj_new, ref_proj_new, depth_values)
+                ref_proj_new[:, :3, :4] = torch.matmul(ref_proj[:, 1, :3, :3],
+                                                       ref_proj[:, 0, :3, :4])
+                warped_volume, proj_mask = homo_warping_3D_with_mask(
+                    src_feat, src_proj_new, ref_proj_new, depth_values)
 
                 B, C, D, H, W = warped_volume.shape
                 G = self.args['base_ch']
@@ -78,8 +97,11 @@ class StageNet(nn.Module):
 
                 if G < C:
                     warped_volume = warped_volume.view(B, G, C // G, D, H, W)
-                    ref_volume = ref_feat.view(B, G, C // G, 1, H, W).repeat(1, 1, 1, D, 1, 1).to(torch.float32)
-                    in_prod_vol = (ref_volume * warped_volume).mean(dim=2)  # [B,G,D,H,W]
+                    ref_volume = ref_feat.view(B, G, C // G, 1, H,
+                                               W).repeat(1, 1, 1, D, 1,
+                                                         1).to(torch.float32)
+                    in_prod_vol = (ref_volume * warped_volume).mean(
+                        dim=2)  # [B,G,D,H,W]
                 elif G == C:
                     ref_volume = ref_feat.view(B, G, 1, H, W).to(torch.float32)
                     in_prod_vol = ref_volume * warped_volume  # [B,C(G),D,H,W]
@@ -89,7 +111,9 @@ class StageNet(nn.Module):
                 if self.fusion_type == 'cnn':
                     sim_vol = in_prod_vol.sum(dim=1)  # [B,D,H,W]
                     sim_vol_norm = F.softmax(sim_vol.detach(), dim=1)
-                    entropy = (- sim_vol_norm * torch.log(sim_vol_norm + 1e-7)).sum(dim=1, keepdim=True)
+                    entropy = (-sim_vol_norm *
+                               torch.log(sim_vol_norm + 1e-7)).sum(
+                                   dim=1, keepdim=True)
                     vis_weight = self.vis(entropy)
                 else:
                     raise NotImplementedError
@@ -98,22 +122,27 @@ class StageNet(nn.Module):
                 vis_sum = vis_sum + vis_weight
 
             # aggregate multiple feature volumes by variance
-            volume_mean = volume_sum / (vis_sum.unsqueeze(1) + 1e-6)  # volume_sum / (num_views - 1)
+            volume_mean = volume_sum / (vis_sum.unsqueeze(1) + 1e-6
+                                        )  # volume_sum / (num_views - 1)
 
         cost_reg = self.cost_reg(volume_mean, position3d)
 
-        prob_volume_pre = cost_reg[:,0,...]
+        prob_volume_pre = cost_reg[:, 0, ...]
         prob_volume = F.softmax(prob_volume_pre, dim=1)
 
         if self.depth_type == 'ce':
             if self.training:
                 _, idx = torch.max(prob_volume, dim=1)
                 # vanilla argmax
-                depth = torch.gather(depth_values, dim=1, index=idx.unsqueeze(1))[:,0,...]
+                depth = torch.gather(depth_values,
+                                     dim=1,
+                                     index=idx.unsqueeze(1))[:, 0, ...]
             else:
                 # regression (t)
-                prob_volume_float = F.softmax(prob_volume_pre * tmp, dim=1).float()
-                depth = depth_regression(prob_volume_float, depth_values=depth_values)
+                prob_volume_float = F.softmax(prob_volume_pre * tmp,
+                                              dim=1).float()
+                depth = depth_regression(prob_volume_float,
+                                         depth_values=depth_values)
             # conf
             photometric_confidence = prob_volume.max(1)[0]  # [B,H,W]
 
@@ -128,7 +157,12 @@ class StageNet(nn.Module):
             else:  # D == 4
                 photometric_confidence = prob_volume.max(1)[0]  # [B,H,W]
 
-        outputs = {'depth': depth, 'prob_volume': prob_volume, "photometric_confidence": photometric_confidence.detach(),
-                   'depth_values': depth_values, 'prob_volume_pre': prob_volume_pre}
+        outputs = {
+            'depth': depth,
+            'prob_volume': prob_volume,
+            "photometric_confidence": photometric_confidence.detach(),
+            'depth_values': depth_values,
+            'prob_volume_pre': prob_volume_pre
+        }
 
         return outputs
--- models/dino/layers/attention.py
+++ models/dino/layers/attention.py
@@ -17,6 +17,7 @@ import torch
 from torch import Tensor
 from torch import nn
 import torch.nn.functional as F
+
 logger = logging.getLogger("dinov2")
 
 try:
@@ -49,20 +50,19 @@ def get_attention_type(attention_type):
 
 
 class Attention(nn.Module):
-    def __init__(
-            self,
-            dim: int,
-            num_heads: int = 8,
-            qkv_bias: bool = False,
-            proj_bias: bool = True,
-            attn_drop: float = 0.0,
-            proj_drop: float = 0.0,
-            **kwargs
-    ) -> None:
+
+    def __init__(self,
+                 dim: int,
+                 num_heads: int = 8,
+                 qkv_bias: bool = False,
+                 proj_bias: bool = True,
+                 attn_drop: float = 0.0,
+                 proj_drop: float = 0.0,
+                 **kwargs) -> None:
         super().__init__()
         self.num_heads = num_heads
         head_dim = dim // num_heads
-        self.scale = head_dim ** -0.5
+        self.scale = head_dim**-0.5
         self.attn_drop_rate = attn_drop
 
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
@@ -73,10 +73,10 @@ class Attention(nn.Module):
         self.softmax_scale = kwargs.get('softmax_scale', None)
         self.train_avg_length = kwargs.get('train_avg_length', None)
 
-
     def forward(self, x: Tensor, return_attn=False) -> Tensor:
         B, N, C = x.shape
-        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads,
+                                  C // self.num_heads).permute(2, 0, 3, 1, 4)
 
         q, k, v = qkv[0], qkv[1], qkv[2]
         softmax_scale = self.scale
@@ -101,10 +101,13 @@ class Attention(nn.Module):
         return x
 
 
-
-
 class MemEffAttention(Attention):
-    def forward(self, x: Tensor, attn_bias=None, return_attn=False, positions=None):
+
+    def forward(self,
+                x: Tensor,
+                attn_bias=None,
+                return_attn=False,
+                positions=None):
         if not XFORMERS_AVAILABLE:
             assert attn_bias is None, "xFormers is required for nested tensors usage"
             return super().forward(x)
@@ -119,7 +122,11 @@ class MemEffAttention(Attention):
         else:
             softmax_scale = self.scale * math.log(N, self.train_avg_length)
 
-        x = memory_efficient_attention(q, k, v, attn_bias=attn_bias, scale=softmax_scale)
+        x = memory_efficient_attention(q,
+                                       k,
+                                       v,
+                                       attn_bias=attn_bias,
+                                       scale=softmax_scale)
         x = x.reshape([B, N, C])
 
         x = self.proj(x)
@@ -130,7 +137,9 @@ class MemEffAttention(Attention):
             q = q.permute(0, 2, 1, 3)
             k = k.permute(0, 2, 1, 3)
             # [B,nhead,1,C]x[B,nhead,C,L-1]=[B,nhead,1,L-1]
-            a = q[:, :, 0:1] @ k[:, :, 1:].transpose(-2, -1) * self.scale if softmax_scale is None else softmax_scale
+            a = q[:, :, 0:1] @ k[:, :, 1:].transpose(
+                -2,
+                -1) * self.scale if softmax_scale is None else softmax_scale
             a = a.squeeze(2)  # [B,nhead,L-1]
             a = torch.softmax(a, dim=2)
             return x, a
@@ -139,7 +148,12 @@ class MemEffAttention(Attention):
 
 
 class FlashAttention2(Attention):
-    def forward(self, x: Tensor, attn_bias=None, positions=None, return_attn=False):
+
+    def forward(self,
+                x: Tensor,
+                attn_bias=None,
+                positions=None,
+                return_attn=False):
 
         if not FLASH_AVAILABLE:
             assert attn_bias is None, "FLASH-ATTENTION2 is required for nested tensors usage"
@@ -161,7 +175,10 @@ class FlashAttention2(Attention):
             softmax_scale = self.scale * math.log(N, self.train_avg_length)
 
         # out: (batch_size, seqlen, nheads, headdim)
-        x = flash_attn_qkvpacked_func(qkv, dropout_p=dropout_p, softmax_scale=softmax_scale, causal=False)
+        x = flash_attn_qkvpacked_func(qkv,
+                                      dropout_p=dropout_p,
+                                      softmax_scale=softmax_scale,
+                                      causal=False)
         x = x.reshape([B, N, C])
 
         x = self.proj(x)
@@ -171,20 +188,19 @@ class FlashAttention2(Attention):
 
 
 class CrossAttention(nn.Module):
-    def __init__(
-            self,
-            dim: int,
-            num_heads: int = 8,
-            qkv_bias: bool = False,
-            proj_bias: bool = True,
-            attn_drop: float = 0.0,
-            proj_drop: float = 0.0,
-            **kwargs
-    ) -> None:
+
+    def __init__(self,
+                 dim: int,
+                 num_heads: int = 8,
+                 qkv_bias: bool = False,
+                 proj_bias: bool = True,
+                 attn_drop: float = 0.0,
+                 proj_drop: float = 0.0,
+                 **kwargs) -> None:
         super().__init__()
         self.num_heads = num_heads
         head_dim = dim // num_heads
-        self.scale = head_dim ** -0.5
+        self.scale = head_dim**-0.5
         self.attn_drop_rate = attn_drop
 
         self.q_proj = nn.Linear(dim, dim, bias=qkv_bias)
@@ -206,7 +222,8 @@ class CrossAttention(nn.Module):
         value = x if value is None else value
         q = self.q_proj(x).reshape(B, N, self.num_heads, C // self.num_heads)
         k = self.k_proj(key).reshape(B, N, self.num_heads, C // self.num_heads)
-        v = self.v_proj(value).reshape(B, N, self.num_heads, C // self.num_heads)
+        v = self.v_proj(value).reshape(B, N, self.num_heads,
+                                       C // self.num_heads)
 
         softmax_scale = self.scale
         if self.softmax_scale == "entropy_invariance":
@@ -225,6 +242,7 @@ class CrossAttention(nn.Module):
 
 
 class CrossFlashAttention2(CrossAttention):
+
     def forward(self, x: Tensor, key=None, value=None, **kwargs):
 
         if not FLASH_AVAILABLE:
@@ -235,7 +253,8 @@ class CrossFlashAttention2(CrossAttention):
         value = x if value is None else value
         q = self.q_proj(x).reshape(B, N, self.num_heads, C // self.num_heads)
         k = self.k_proj(key).reshape(B, N, self.num_heads, C // self.num_heads)
-        v = self.v_proj(value).reshape(B, N, self.num_heads, C // self.num_heads)
+        v = self.v_proj(value).reshape(B, N, self.num_heads,
+                                       C // self.num_heads)
 
         # dropout_p: float. Dropout probability. use 0 for inference
         if self.training:
@@ -249,7 +268,12 @@ class CrossFlashAttention2(CrossAttention):
             softmax_scale = self.scale * math.log(N, self.train_avg_length)
 
         # out: (batch_size, seqlen, nheads, headdim)
-        x = flash_attn_func(q, k, v, dropout_p=dropout_p, softmax_scale=softmax_scale, causal=False)
+        x = flash_attn_func(q,
+                            k,
+                            v,
+                            dropout_p=dropout_p,
+                            softmax_scale=softmax_scale,
+                            causal=False)
         x = x.reshape([B, N, C])
 
         x = self.proj(x)
@@ -259,22 +283,26 @@ class CrossFlashAttention2(CrossAttention):
 
 
 class CrossLinearAttention(CrossAttention):
+
     def forward(self, x: Tensor, key=None, value=None, **kwargs):
         eps = 1e-6
 
         B, N, C = x.shape
         key = x if key is None else key
         value = x if value is None else value
-        q = self.q_proj(x).reshape(B, N, self.num_heads, C // self.num_heads).to(dtype=torch.float32)
-        k = self.k_proj(key).reshape(B, N, self.num_heads, C // self.num_heads).to(dtype=torch.float32)
-        v = self.v_proj(value).reshape(B, N, self.num_heads, C // self.num_heads).to(dtype=torch.float32)
+        q = self.q_proj(x).reshape(B, N, self.num_heads,
+                                   C // self.num_heads).to(dtype=torch.float32)
+        k = self.k_proj(key).reshape(B, N, self.num_heads, C //
+                                     self.num_heads).to(dtype=torch.float32)
+        v = self.v_proj(value).reshape(B, N, self.num_heads, C //
+                                       self.num_heads).to(dtype=torch.float32)
         q = torch.nn.functional.elu(q) + 1
         k = torch.nn.functional.elu(k) + 1
 
         # Compute the KV matrix, namely the dot product of keys and values so
         # that we never explicitly compute the attention matrix and thus
         # decrease the complexity [B,L,nh,C]x[B,L,nh,C]->[B,nh,C,C]
-        with torch.cuda.amp.autocast(enabled=False):
+        with torch.amp.autocast('cuda', enabled=False):
             KV = torch.einsum("nshd,nshm->nhmd", k, v)
 
             # Compute the normalizer [B,L,nh,C]x[B,nh,C]->[B,L,nh]
@@ -292,7 +320,13 @@ class CrossLinearAttention(CrossAttention):
 
 
 class CrossXFormersAttention(Attention):
-    def forward(self, x: Tensor, key=None, value=None, attn_bias=None, **kwargs):
+
+    def forward(self,
+                x: Tensor,
+                key=None,
+                value=None,
+                attn_bias=None,
+                **kwargs):
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads)
 
@@ -304,8 +338,13 @@ class CrossXFormersAttention(Attention):
             softmax_scale = self.scale * math.log(N, self.train_avg_length)
 
         if attn_bias is not None:
-            attn_bias = attn_bias.unsqueeze(1).repeat(1, self.num_heads, 1, 1).to(q.dtype)  # [B,HW,HW]->[B,nh,HW,HW]
-        x = memory_efficient_attention(q, k, v, attn_bias=attn_bias, scale=softmax_scale)
+            attn_bias = attn_bias.unsqueeze(1).repeat(
+                1, self.num_heads, 1, 1).to(q.dtype)  # [B,HW,HW]->[B,nh,HW,HW]
+        x = memory_efficient_attention(q,
+                                       k,
+                                       v,
+                                       attn_bias=attn_bias,
+                                       scale=softmax_scale)
         x = x.reshape([B, N, C])
 
         x = self.proj(x)
@@ -314,10 +353,9 @@ class CrossXFormersAttention(Attention):
         return x
 
 
-
 if __name__ == '__main__':
-    N= 128
-    x = torch.rand(1,N,64)
+    N = 128
+    x = torch.rand(1, N, 64)
     Atten = Attention(dim=64)
     train_avg_length = 64
     scale = Atten.scale * math.log(N, train_avg_length)
